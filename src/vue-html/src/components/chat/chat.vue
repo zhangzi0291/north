b403fd016d4e9d71e64729bcb4fe5dad3d9de090
North_ZX <zhangszi0291>
2018-07-04 18:06:20 +0800

    调整聊天界面

--- src/vue-html/src/components/chat/chat.vue
+++ src/vue-html/src/components/chat/chat.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Row type="flex" justify="center" align="middle">
+        <Row type="flex" justify="center" align="middle" :style="{height: msgBoxHeight}">
             <ul>
                 <li v-for="item in context">
                     {{item.auth}}--{{item.msg}}
@@ -8,10 +8,10 @@
             </ul>
         </Row>
         <Row type="flex" justify="center" align="middle">
-            <i-input v-model="msg"></i-input>
+            <Input v-model="msg" class="sendInput sendBox" size="large"></Input><Button @click='send'  class="sendButton sendBox" type="primary" size="large">发送</Button>
         </Row>
-        <Row type="flex" justify="center" align="middle">
-            <Col span="8">
+        <Row type="flex" justify="center" align="middle" >
+            <!--<Col span="8">
             <i-button @click='send'>send</i-button>
             </Col>
             <Col span="8">
@@ -19,7 +19,7 @@
             </Col>
             <Col span="8">
             <i-button @click='close'>close</i-button>
-            </Col>
+            </Col>-->
         </Row>
     </div>
 </template>
@@ -29,50 +29,65 @@ export default {
     data() {
         return {
             msg: '123',
+            msgBoxHeight:"600px",
             context: []
         }
     },
     mounted() {
         const $this = this;
-        window.onbeforeunload = function(evt) {
-            $this.$Modal.error({
-                title: "网络错误",
-                content: "连接建立失败"
-            });
-        }
-        ws.onopen = function(evt) {
-            console.log(evt)
-            console.log("Connection open ...");
-        };
 
-        ws.onmessage = function(evt) {
-            console.log("Received Message: " + evt.data);
-            $this.context.push(JSON.parse(evt.data))
-        };
-        ws.onerror = function(evt) {
-            console.log("Connection error");
-            $this.$Modal.error({
-                title: "网络错误",
-                content: "连接服务器失败"
-            });
-        };
-        ws.onclose = function(evt) {
-            console.log("Connection closed.");
-        };
+        const inputHeight = 46;
+        this.msgBoxHeight = window.document.documentElement.clientHeight-inputHeight+"px"
+        window.onresize = () =>{
+             this.msgBoxHeight = window.document.documentElement.clientHeight-inputHeight+"px"
+        }
+        // window.onbeforeunload = function(evt) {
+        //     $this.$Modal.error({
+        //         title: "网络错误",
+        //         content: "连接建立失败"
+        //     });
+        // }
+        // ws.onopen = function(evt) {
+        //     console.log(evt)
+        //     console.log("Connection open ...");
+        // };
+
+        // ws.onmessage = function(evt) {
+        //     console.log("Received Message: " + evt.data);
+        //     $this.context.push(JSON.parse(evt.data))
+        // };
+        // ws.onerror = function(evt) {
+        //     console.log("Connection error");
+        //     $this.$Modal.error({
+        //         title: "网络错误",
+        //         content: "连接服务器失败"
+        //     });
+        // };
+        // ws.onclose = function(evt) {
+        //     console.log("Connection closed.");
+        // };
     },
     methods: {
-        send: function() {
-            ws.send(this.msg);
-        },
-        restart: function() {
-            console.log("restart")
-        },
-        close: function() {
-            ws.close();
-        }
+        // send: function() {
+        //     ws.send(this.msg);
+        // },
+        // restart: function() {
+        //     console.log("restart")
+        // },
+        // close: function() {
+        //     ws.close();
+        // }
     }
 }
 </script>
 <style scoped>
-
+.sendBox{
+    margin: 5px;
+}
+.sendInput{
+    width: calc(100% - 100px );
+}
+.sendButton{
+    width: 80px;
+}
 </style>
