68b89a31d6c259290db610344ff22b0362ab496f
north_zx <zhangszi0291@gmail.com>
2020-08-29 12:30:58 +0800

    update

--- src/vue-html/src/components/sys/log/formModal.vue
+++ src/vue-html/src/components/sys/log/formModal.vue
@@ -15,36 +15,36 @@
           <Form :model="data"  inline>
             <Row>
                 <Col span="12">
-                    <FormItem label="操作时间：" :label-width="labelwidth">
+                    <FormItem label="操作时间：" :label-width="labelwidth" prop="createTime">
                         <Input v-model="data.createTime" placeholder="" clearable/>
                     </FormItem>
                 </Col>
                 <Col span="12">
-                    <FormItem label="IP：" :label-width="labelwidth">
+                    <FormItem label="IP：" :label-width="labelwidth" prop="ip">
                         <Input v-model="data.ip" placeholder="" clearable/>
                     </FormItem>
                 </Col>
             </Row>
             <Row>
                 <Col span="12">
-                    <FormItem label="模块名称：" :label-width="labelwidth">
+                    <FormItem label="模块名称：" :label-width="labelwidth" prop="moduleName">
                         <Input v-model="data.moduleName" placeholder="" clearable/>
                     </FormItem>
                 </Col>
                 <Col span="12">
-                    <FormItem label="操作名称：" :label-width="labelwidth">
+                    <FormItem label="操作名称：" :label-width="labelwidth" prop="operationName">
                         <Input v-model="data.operationName" placeholder="" clearable/>
                     </FormItem>
                 </Col>
             </Row>
             <Row>
                 <Col span="12">
-                    <FormItem label="操作用户：" :label-width="labelwidth">
+                    <FormItem label="操作用户：" :label-width="labelwidth" prop="username">
                         <Input v-model="data.username" placeholder="" clearable/>
                     </FormItem>
                 </Col>
                 <Col span="12">
-                    <FormItem label="备注：" :label-width="labelwidth">
+                    <FormItem label="备注：" :label-width="labelwidth" prop="remark">
                         <Input v-model="data.remark" placeholder="" clearable/>
                     </FormItem>
                 </Col>
@@ -52,6 +52,10 @@
 
           </Form>
       </Row>
+      <div slot="footer">
+          <Button @click="show=false">取消</Button>
+          <Button type="primary" @click="ok()">确定</Button>
+      </div>
   </Modal>
 </template>
 <script>
@@ -88,6 +92,12 @@ export default {
       type: String,
       default: ""
     },
+    okCallback:{
+        type: Function,
+        default: function(){
+            
+        }
+    },
   },
   filters:{
     
@@ -99,29 +109,32 @@ export default {
     //提交
     ok: function() {
       let $this = this;
-    //可以做校验
-    //   if (this.$refs.editModal.validateForm()) {
-    //     $this.addshow = false;
-    //     setTimeout(function() {
-    //       $this.addshow = true;
-    //     }, 1);
-    //     return;
-    //   }
+      // let flag = false;
+      // //校验
+      // this.$refs.form.validate(valid => {
+      //     flag = valid
+      // });
+      // if(!flag){
+      //     this.show = true
+      //     return
+      // }
       this.$ajax({
         method: "post",
         url: this.url,
         data: this.data
       }).then(function(res) {
         $this.successModal("操作成功");
+        $this.okCallback()
         //刷新表格
         $this.$parent.$refs.table.searchData();
+        this.show = false
       });
     },
     //弹出或关闭模态窗
     changeShow(id){
         this.id = id;
         console.log(this.id)
-        this.show = !this.show
+        this.show = true
         if(!!this.id){
             this.$ajax({
                 method: "post",
